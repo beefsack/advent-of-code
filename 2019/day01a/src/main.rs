7083e5b5598048260b060a5170f27fb3381c9e00
Michael Alexander <beefsack@gmail.com>
2019-12-02 13:41:36 +1100

    2019/day1a completed

--- 2019/day01a/src/main.rs
+++ 2019/day01a/src/main.rs
@@ -1,3 +1,42 @@
+use anyhow::Result;
+use std::io::{self, BufRead};
+
 fn main() {
-    println!("Hello, world!");
+    print!(
+        "{}",
+        read_usizes()
+            .unwrap()
+            .into_iter()
+            .map(required_fuel)
+            .sum::<usize>()
+    );
+}
+
+fn read_usizes() -> Result<Vec<usize>> {
+    io::stdin().lock().lines().map(|l| parse_line(l?)).collect()
+}
+
+fn parse_line(line: String) -> Result<usize> {
+    Ok(line.parse()?)
+}
+
+fn required_fuel(mass: usize) -> usize {
+    if mass > 2 {
+        mass / 3 - 2
+    } else {
+        0
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_required_fuel() {
+        assert_eq!(required_fuel(12), 2);
+        assert_eq!(required_fuel(14), 2);
+        assert_eq!(required_fuel(1969), 654);
+        assert_eq!(required_fuel(100756), 33583);
+    }
 }
