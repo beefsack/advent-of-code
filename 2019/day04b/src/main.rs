d89a75f6561895be7c80254110585758e261de72
Michael Alexander <beefsack@gmail.com>
2019-12-04 17:45:41 +1100

    2019/day04b completed

--- 2019/day04b/src/main.rs
+++ 2019/day04b/src/main.rs
@@ -17,7 +17,8 @@ fn is_valid(pass: usize) -> bool {
         return false;
     }
     let mut last: Option<char> = None;
-    let mut has_consecutive = false;
+    let mut consecutives: Vec<usize> = vec![];
+    let mut cur_consecutive: usize = 0;
     for c in pass.to_string().chars() {
         if let Some(last_c) = last {
             if c < last_c {
@@ -25,12 +26,18 @@ fn is_valid(pass: usize) -> bool {
                 return false;
             }
             if c == last_c {
-                has_consecutive = true;
+                cur_consecutive += 1;
+            } else {
+                consecutives.push(cur_consecutive);
+                cur_consecutive = 1;
             }
+        } else {
+            cur_consecutive += 1;
         }
         last = Some(c);
     }
-    has_consecutive
+    consecutives.push(cur_consecutive);
+    consecutives.iter().any(|&c| c == 2)
 }
 
 #[cfg(test)]
@@ -39,8 +46,8 @@ mod tests {
 
     #[test]
     fn test_is_valid() {
-        assert_eq!(is_valid(111111), true);
-        assert_eq!(is_valid(223450), false);
-        assert_eq!(is_valid(123789), false);
+        assert_eq!(is_valid(112233), true);
+        assert_eq!(is_valid(123444), false);
+        assert_eq!(is_valid(111122), true);
     }
 }
