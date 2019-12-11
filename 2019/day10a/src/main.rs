18b42644a7b05dd8046541437cfad2cd75f366c0
Michael Alexander <beefsack@gmail.com>
2019-12-11 13:07:21 +1100

    2019/day10a completed

---
+++ 2019/day10a/src/main.rs
@@ -0,0 +1,431 @@
+use anyhow::{anyhow, Result};
+use num;
+
+use std::collections::{HashMap, HashSet};
+use std::io::{stdin, Read};
+use std::iter::FromIterator;
+use std::ops::{Add, Sub};
+
+#[derive(Debug, PartialEq, Eq)]
+enum MapItem {
+    Empty,
+    Asteroid,
+}
+
+const MAP_ITEM_EMPTY_CHAR: char = '.';
+const MAP_ITEM_ASTEROID_CHAR: char = '#';
+
+impl MapItem {
+    fn from_char(c: char) -> Result<Self> {
+        match c {
+            MAP_ITEM_EMPTY_CHAR => Ok(MapItem::Empty),
+            MAP_ITEM_ASTEROID_CHAR => Ok(MapItem::Asteroid),
+            _ => Err(anyhow!("'{}' not a valid map item", c)),
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, Eq)]
+struct Map(Vec<Vec<MapItem>>);
+
+impl Map {
+    fn parse(input: &str) -> Result<Self> {
+        Ok(Self(
+            input
+                .trim()
+                .lines()
+                .map(Map::parse_line)
+                .collect::<Result<Vec<Vec<MapItem>>>>()?,
+        ))
+    }
+
+    fn parse_line(input: &str) -> Result<Vec<MapItem>> {
+        input.chars().map(MapItem::from_char).collect()
+    }
+
+    fn find(&self, kind: &MapItem) -> Vec<Point> {
+        self.0
+            .iter()
+            .enumerate()
+            .map(|(y, row)| {
+                row.iter()
+                    .enumerate()
+                    .filter_map(|(x, item)| {
+                        if item == kind {
+                            Some(Point {
+                                x: x as isize,
+                                y: y as isize,
+                            })
+                        } else {
+                            None
+                        }
+                    })
+                    .collect::<Vec<Point>>()
+            })
+            .flatten()
+            .collect()
+    }
+
+    fn visible_pairs(&self) -> Vec<[Point; 2]> {
+        let asteroids = self.find(&MapItem::Asteroid);
+        let asteroids_set: HashSet<Point> = HashSet::from_iter(asteroids.to_owned());
+        let mut pairs: Vec<[Point; 2]> = vec![];
+        for (index, a1) in asteroids.iter().enumerate().take(asteroids.len() - 1) {
+            'a2: for a2 in asteroids.iter().skip(index + 1) {
+                let step = (*a2 - *a1).smallest_whole_vector();
+                let mut ptr = *a1 + step;
+                while ptr != *a2 {
+                    if asteroids_set.contains(&ptr) {
+                        // There is an asteroid in the way, skip this pair
+                        continue 'a2;
+                    }
+                    ptr = ptr + step;
+                }
+                pairs.push([*a1, *a2]);
+            }
+        }
+        pairs
+    }
+
+    fn most_visible(&self) -> (Vec<Point>, usize) {
+        let mut asteroid_visible: HashMap<Point, usize> = HashMap::new();
+        let mut most = 0;
+        for pair in self.visible_pairs() {
+            for a in pair.iter() {
+                let e = asteroid_visible.entry(*a).or_default();
+                *e += 1;
+                if *e > most {
+                    most = *e;
+                }
+            }
+        }
+        (
+            asteroid_visible
+                .into_iter()
+                .filter_map(|(a, n)| if n == most { Some(a) } else { None })
+                .collect(),
+            most,
+        )
+    }
+}
+
+#[derive(Debug, PartialEq, Eq, Hash, Copy, Clone)]
+struct Point {
+    x: isize,
+    y: isize,
+}
+
+impl Sub for Point {
+    type Output = Self;
+
+    fn sub(self, other: Point) -> Self {
+        Point {
+            x: self.x - other.x,
+            y: self.y - other.y,
+        }
+    }
+}
+
+impl Add for Point {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self {
+        Self {
+            x: self.x + other.x,
+            y: self.y + other.y,
+        }
+    }
+}
+
+impl Point {
+    fn smallest_whole_vector(self) -> Point {
+        match self {
+            Point { x: 0, y: 0 } => self,
+            Point { x, y: 0 } => Point {
+                x: x.signum(),
+                y: 0,
+            },
+            Point { x: 0, y } => Point {
+                x: 0,
+                y: y.signum(),
+            },
+            Point { x, y } => {
+                let gcd = num::integer::gcd(x, y);
+                Point {
+                    x: x / gcd,
+                    y: y / gcd,
+                }
+            }
+        }
+    }
+}
+
+fn main() -> Result<()> {
+    let mut input = String::new();
+    stdin().lock().read_to_string(&mut input)?;
+    let map = Map::parse(&input)?;
+    let (_, count) = map.most_visible();
+    println!("{}", count);
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use anyhow::Result;
+
+    #[test]
+    fn test_map_item_from_char() -> Result<()> {
+        assert_eq!(MapItem::from_char(MAP_ITEM_EMPTY_CHAR)?, MapItem::Empty);
+        assert_eq!(
+            MapItem::from_char(MAP_ITEM_ASTEROID_CHAR)?,
+            MapItem::Asteroid
+        );
+        assert_eq!(MapItem::from_char('z').is_err(), true);
+        Ok(())
+    }
+
+    #[test]
+    fn test_map_parse_line() -> Result<()> {
+        assert_eq!(
+            Map::parse_line(".#..#")?,
+            vec![
+                MapItem::Empty,
+                MapItem::Asteroid,
+                MapItem::Empty,
+                MapItem::Empty,
+                MapItem::Asteroid,
+            ]
+        );
+        Ok(())
+    }
+
+    #[test]
+    fn test_map_parse() -> Result<()> {
+        assert_eq!(
+            Map::parse(
+                "
+.#..#
+.....
+#####
+....#
+...##
+"
+            )?,
+            Map(vec![
+                vec![
+                    MapItem::Empty,
+                    MapItem::Asteroid,
+                    MapItem::Empty,
+                    MapItem::Empty,
+                    MapItem::Asteroid,
+                ],
+                vec![
+                    MapItem::Empty,
+                    MapItem::Empty,
+                    MapItem::Empty,
+                    MapItem::Empty,
+                    MapItem::Empty,
+                ],
+                vec![
+                    MapItem::Asteroid,
+                    MapItem::Asteroid,
+                    MapItem::Asteroid,
+                    MapItem::Asteroid,
+                    MapItem::Asteroid,
+                ],
+                vec![
+                    MapItem::Empty,
+                    MapItem::Empty,
+                    MapItem::Empty,
+                    MapItem::Empty,
+                    MapItem::Asteroid,
+                ],
+                vec![
+                    MapItem::Empty,
+                    MapItem::Empty,
+                    MapItem::Empty,
+                    MapItem::Asteroid,
+                    MapItem::Asteroid,
+                ],
+            ]),
+        );
+        Ok(())
+    }
+
+    #[test]
+    fn test_map_find() -> Result<()> {
+        let map = Map::parse(
+            "
+.#.
+...
+###
+",
+        )?;
+        assert_eq!(
+            map.find(&MapItem::Asteroid),
+            vec![
+                Point { x: 1, y: 0 },
+                Point { x: 0, y: 2 },
+                Point { x: 1, y: 2 },
+                Point { x: 2, y: 2 },
+            ]
+        );
+        Ok(())
+    }
+
+    #[test]
+    fn test_map_visible_pairs() -> Result<()> {
+        let map = Map::parse(
+            "
+##.
+.#.
+###
+",
+        )?;
+        assert_eq!(
+            map.visible_pairs(),
+            vec![
+                [Point { x: 0, y: 0 }, Point { x: 1, y: 0 }],
+                [Point { x: 0, y: 0 }, Point { x: 1, y: 1 }],
+                [Point { x: 0, y: 0 }, Point { x: 0, y: 2 }],
+                [Point { x: 0, y: 0 }, Point { x: 1, y: 2 }],
+                [Point { x: 1, y: 0 }, Point { x: 1, y: 1 }],
+                [Point { x: 1, y: 0 }, Point { x: 0, y: 2 }],
+                [Point { x: 1, y: 0 }, Point { x: 2, y: 2 }],
+                [Point { x: 1, y: 1 }, Point { x: 0, y: 2 }],
+                [Point { x: 1, y: 1 }, Point { x: 1, y: 2 }],
+                [Point { x: 1, y: 1 }, Point { x: 2, y: 2 }],
+                [Point { x: 0, y: 2 }, Point { x: 1, y: 2 }],
+                [Point { x: 1, y: 2 }, Point { x: 2, y: 2 }],
+            ]
+        );
+        Ok(())
+    }
+
+    #[test]
+    fn test_point_smallest_whole_vector() {
+        assert_eq!(
+            Point { x: 0, y: 0 }.smallest_whole_vector(),
+            Point { x: 0, y: 0 }
+        );
+        assert_eq!(
+            Point { x: 0, y: 5 }.smallest_whole_vector(),
+            Point { x: 0, y: 1 }
+        );
+        assert_eq!(
+            Point { x: 0, y: -5 }.smallest_whole_vector(),
+            Point { x: 0, y: -1 }
+        );
+        assert_eq!(
+            Point { x: 5, y: 0 }.smallest_whole_vector(),
+            Point { x: 1, y: 0 }
+        );
+        assert_eq!(
+            Point { x: -5, y: 0 }.smallest_whole_vector(),
+            Point { x: -1, y: 0 }
+        );
+        assert_eq!(
+            Point { x: 3, y: 3 }.smallest_whole_vector(),
+            Point { x: 1, y: 1 }
+        );
+        assert_eq!(
+            Point { x: 4, y: 6 }.smallest_whole_vector(),
+            Point { x: 2, y: 3 }
+        );
+        assert_eq!(
+            Point { x: 7, y: -10 }.smallest_whole_vector(),
+            Point { x: 7, y: -10 }
+        );
+    }
+
+    #[test]
+    fn test_map_most_visible() -> Result<()> {
+        let mut map = Map::parse(
+            "
+.#..#
+.....
+#####
+....#
+...##
+",
+        )?;
+        assert_eq!(map.most_visible(), (vec![Point { x: 3, y: 4 }], 8));
+
+        map = Map::parse(
+            "
+......#.#.
+#..#.#....
+..#######.
+.#.#.###..
+.#..#.....
+..#....#.#
+#..#....#.
+.##.#..###
+##...#..#.
+.#....####
+",
+        )?;
+        assert_eq!(map.most_visible(), (vec![Point { x: 5, y: 8 }], 33));
+
+        map = Map::parse(
+            "
+#.#...#.#.
+.###....#.
+.#....#...
+##.#.#.#.#
+....#.#.#.
+.##..###.#
+..#...##..
+..##....##
+......#...
+.####.###.
+",
+        )?;
+        assert_eq!(map.most_visible(), (vec![Point { x: 1, y: 2 }], 35));
+
+        map = Map::parse(
+            "
+.#..#..###
+####.###.#
+....###.#.
+..###.##.#
+##.##.#.#.
+....###..#
+..#.#..#.#
+#..#.#.###
+.##...##.#
+.....#.#..
+",
+        )?;
+        assert_eq!(map.most_visible(), (vec![Point { x: 6, y: 3 }], 41));
+
+        map = Map::parse(
+            "
+.#..##.###...#######
+##.############..##.
+.#.######.########.#
+.###.#######.####.#.
+#####.##.#.##.###.##
+..#####..#.#########
+####################
+#.####....###.#.#.##
+##.#################
+#####.##.###..####..
+..######..##.#######
+####.##.####...##..#
+.#####..#.######.###
+##...#.##########...
+#.##########.#######
+.####.#.###.###.#.##
+....##.##.###..#####
+.#.#.###########.###
+#.#.#.#####.####.###
+###.##.####.##.#..##
+",
+        )?;
+        assert_eq!(map.most_visible(), (vec![Point { x: 11, y: 13 }], 210));
+
+        Ok(())
+    }
+}
